9085a84820eef1e56d7defaaf8c1df5386f13cca
JURGEN VOLDERS <Jurgenvolders@hotmail.com>
2021-03-19 13:25:42 +0100

    Async

--- BrouwerService/Controllers/BrouwerController.cs
+++ BrouwerService/Controllers/BrouwerController.cs
@@ -1,6 +1,8 @@
-﻿using BrouwerService.Repositories;
+﻿using BrouwerService.Models;
+using BrouwerService.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,14 +18,61 @@ namespace BrouwerService.Controllers
         public BrouwerController(IBrouwerRepository repository)
         { this.repository = repository; }
         [HttpGet]
-        public ActionResult FindAll() { return base.Ok(repository.FindAll()); }
+        public async Task<ActionResult> FindAll() => base.Ok(await repository.FindAllAsync());
         [HttpGet("{id}")]
-        public ActionResult FindById(int id)
+        public async Task<ActionResult> FindById(int id)
         {
-            var brouwer = repository.FindById(id);
+            var brouwer = await repository.FindByIdAsync(id);
             return brouwer == null ? base.NotFound() : base.Ok(brouwer);
         }
-        [HttpGet("naam")]
-        public ActionResult FindByBeginNaam(string begin) =>base.Ok(repository.FindByBeginNaam(begin));
+        [
+      HttpGet("naam")]
+        public async Task<ActionResult> FindByBeginNaam(string begin) =>
+      base.Ok(await repository.FindByBeginNaamAsync(begin));
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var brouwer = await repository.FindByIdAsync(id);
+            if (brouwer == null)
+            {
+                return base.NotFound();
+            }
+            await repository.DeleteAsync(brouwer);
+            return base.Ok();
+        }
+        [
+      HttpPost]
+        public async Task<ActionResult> Post(Brouwer brouwer)
+        {
+            if (this.ModelState.IsValid)
+            {
+                await repository.InsertAsync(brouwer);
+                return base.CreatedAtAction(nameof(FindById), new { id = brouwer.Id }, null);
+            }
+            return base.BadRequest(this.ModelState);
+        }
+        [
+      HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, Brouwer brouwer)
+        {
+            if (this.ModelState.IsValid)
+            {
+                try
+                {
+                    brouwer.Id = id;
+                    await repository.UpdateAsync(brouwer);
+                    return base.Ok();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return base.NotFound();
+                }
+                catch
+                {
+                    return base.Problem();
+                }
+            }
+            return base.BadRequest(this.ModelState);
+        }
     }
 }
--- BrouwerService/Models/Brouwer.cs
+++ BrouwerService/Models/Brouwer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BrouwerService.Models
 {
@@ -12,9 +13,14 @@ namespace BrouwerService.Models
         [DataMember(Name = "id", Order = 1)]
         public int Id { get; set; }
         [DataMember(Name = "naam", Order = 2)]
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         public string Naam { get; set; }
+        [Range(1000, 9999)]
         [DataMember(Name = "postcode", Order = 3)]
         public int Postcode { get; set; }
+        [Required]
+        [StringLength(255, MinimumLength = 1)]
         [DataMember(Name = "gemeente", Order = 4)]
         public string Gemeente { get; set; }
     }
